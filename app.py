1d602f3aae8c8a2e10e44d3794466e8bb38561f6
lelethu <lkosana.kosana@gmail.com>
2025-06-07 17:23:07 +0200

    Initial commit with Helm chart and Python app

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World from Python!"
+    return "Hello from Kubernetes!"
 
+<<<<<<< HEAD
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+=======
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+>>>>>>> 40a1578 (Initial working Kubernetes deployment of Hello Python app)
 
